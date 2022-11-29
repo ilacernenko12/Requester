8423d8d4c2bb6efd6c9dc5a3bec742e71fc3c817
ilacernenko12 <ilacernenko12@gmail.com>
2022-11-29 17:58:58 +0300

    up

--- app/src/main/java/com/example/myapplication/activity/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/activity/SecondActivity.java
@@ -3,14 +3,50 @@ package com.example.myapplication.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.myapplication.R;
+import com.example.myapplication.retrofit.InputData;
+import com.example.myapplication.retrofit.RetrofitFactory;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private EditText editName;
+    private EditText editEmail;
+    private EditText editPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        editName = findViewById(R.id.editName);
+        editEmail = findViewById(R.id.editEmail);
+        editPhone = findViewById(R.id.editPhone);
+    }
+
+
+    public void sendClickOnBack(View view) {
+        String name = editName.getText().toString();
+        String email = editEmail.getText().toString();
+        String phone = editPhone.getText().toString();
+
+        Call<InputData> data = RetrofitFactory.getInstance().setInputData(name, email, phone);
+        data.enqueue(new Callback<InputData>() {
+            @Override
+            public void onResponse(Call<InputData> call, Response<InputData> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<InputData> call, Throwable t) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/retrofit/RetroInterface.java
+++ app/src/main/java/com/example/myapplication/retrofit/RetroInterface.java
@@ -1,7 +1,12 @@
 package com.example.myapplication.retrofit;
 
+import java.util.List;
+
 import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface RetroInterface {
@@ -10,4 +15,9 @@ public interface RetroInterface {
 
     @GET ("get_email")
     Call<String> getEmail(@Query("number") String number);
+
+    @POST ("set_android_data")
+    Call<InputData> setInputData (@Body InputData inputData);
+
+    Call<InputData> setInputData(String name, String email, String phone);
 }
